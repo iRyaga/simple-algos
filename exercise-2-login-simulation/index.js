028b6396efca552c46b974c7d0d0041caa780768
iRyaga <aittalbeayoub@gmail.com>
2026-04-07 14:18:27 -0300

    Add second exercise

---
+++ exercise-2-login-simulation/index.js
@@ -0,0 +1,23 @@
+let username = "ayoubyou";
+let password = "123456789123";
+let attempts = 0;
+let isLoggedIn = false;
+while(!isLoggedIn && attempts < 3){
+    let enteredUsername = "ayoubyouu";
+    let enteredPassword = "12345678912";
+    if (enteredUsername === username && enteredPassword === password){
+        isLoggedIn = true;
+        console.log("Access Granted")
+    }
+    else {
+        attempts++;
+        console.log("Wrong Credentials")
+    }
+
+    }
+    if (!isLoggedIn) {
+    console.log("Account Locked");
+}
+
+
+
